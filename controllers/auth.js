ac64b60751ce4cadc56a3df28367f673bf4936db
Tony Gadaleta <anthonygadaleta@gmail.com>
2024-11-18 12:20:47 -0500

    validation and authentication is working for new users

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import express from "express";
 import User from "../models/user.js";
+import bcrypt from 'bcrypt';
 const router = express.Router();
 
 router.get("/sign-up", (req, res) => {
@@ -9,21 +10,27 @@ router.get("/sign-up", (req, res) => {
 router.post("/sign-up", async (req, res) => {
   try {
     //Grab form data from req.body
-    const { username, password, confirmPassword } = req.body;
+    let { username, password, confirmPassword } = req.body;
 
     //validate password and confirm field match
     if (password!=confirmPassword){
         throw new Error("Passwords don't match.")
     }
+    //validate complexity of password
+    if(!/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]+$/.test(password)){
+        throw new Error("Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")
+    }
     //check if username provided already exists
-    const existingUser = await User.find({username: username})
+    const existingUser = await User.findOne({username: username})
     if (existingUser){
         throw new Error(`User ${username} already exists.`)
     }
+    //encrypt password
+    password = bcrypt.hashSync(password, 10)
     //creates new user
     const newUser = await User.create({username,password})
     if(newUser) {
-        res.redirect('/auth/login');
+        res.redirect('/auth/sign-in');
     }
   } catch (error) {
     console.error(error);
--- models/user.js
+++ models/user.js
@@ -12,11 +12,6 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
         minLength: 6,
-        validate: {
-            validator: (value) =>
-              /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]+$/.test(value),
-            message: 'Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.',
-          },
     }
 })
 
